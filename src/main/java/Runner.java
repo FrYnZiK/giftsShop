a233172845921a2a8eb8c17de574e5245b289270
IvanBuldenko <ivanbwork1@gmail.com>
2021-05-15 22:59:51 +0300

    added tests

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,52 +1,9 @@
-import testgroup.giftography.instances.Client;
-import testgroup.giftography.instances.Order;
-import testgroup.giftography.instances.OrderStatus;
-import testgroup.giftography.jdbc.SQLDatabaseConnection;
-import testgroup.giftography.repository.ClientRepository;
-import testgroup.giftography.repository.OrderRepository;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.UUID;
+import testgroup.giftography.repository.test.ClientRepositryTest;
 
 public class Runner {
     public static void main(String[] args) {
-        Connection connection = new SQLDatabaseConnection().getConnection();
-        ClientRepository clientRepository = new ClientRepository(connection);
-
-        try {
-            Client client = getStaticClient();
-            clientRepository.updateClient(client);
-            Client client1 = clientRepository.getClient(1);
-            System.out.println(client1);
-
-            Client client2 = new Client(
-                    "UUID.randomUUID().toString()",
-                    "Hleb",
-                    "Minsk",
-                    "90100",
-                    UUID.randomUUID().toString() + "@rb.by");
-
-            clientRepository.addClient(client2);
-//            clientRepository.deleteClient(4);
-            Order order = new Order(1, OrderStatus.NEW);
-            new OrderRepository(connection).addOrder(order);
-
-        } catch (SQLException throwables) {
-            System.err.println("ALERT!!!");
-            throwables.printStackTrace();
-        }
+        new ClientRepositryTest().clientTest();
     }
 
-    private static Client getStaticClient() {
-        Client client = new Client(
-                4,
-                "Vitaliy",
-                "Any",
-                "addr",
-                "103",
-                "any@mail.com");
-        return client;
-    }
 
 }
---
+++ src/main/java/testgroup/giftography/repository/test/ClientRepositryTest.java
@@ -0,0 +1,57 @@
+package testgroup.giftography.repository.test;
+
+import testgroup.giftography.instances.Client;
+import testgroup.giftography.instances.Order;
+import testgroup.giftography.instances.OrderStatus;
+import testgroup.giftography.jdbc.SQLDatabaseConnection;
+import testgroup.giftography.repository.ClientRepository;
+import testgroup.giftography.repository.OrderRepository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class ClientRepositryTest {
+
+    Connection connection = new SQLDatabaseConnection().getConnection();
+    ClientRepository clientRepository = new ClientRepository(connection);
+
+    public void clientTest() {
+        try {
+            Client client = getStaticClient();
+            clientRepository.updateClient(client);
+            Client client1 = clientRepository.getClient(1);
+            System.out.println(client1);
+
+            Client client2 = new Client(
+                    "UUID.randomUUID().toString()",
+                    "Hleb",
+                    "Minsk",
+                    "90100",
+                    UUID.randomUUID().toString() + "@rb.by");
+
+            clientRepository.addClient(client2);
+//            clientRepository.deleteClient(4);
+            Order order = new Order(1, OrderStatus.NEW);
+            new OrderRepository(connection).addOrder(order);
+
+        } catch (SQLException throwables) {
+            System.err.println("ALERT!!!");
+            throwables.printStackTrace();
+        }
+
+    }
+
+
+    private static Client getStaticClient() {
+        Client client = new Client(
+                4,
+                "Vitaliy",
+                "Any",
+                "addr",
+                "103",
+                "any@mail.com");
+        return client;
+    }
+
+}
