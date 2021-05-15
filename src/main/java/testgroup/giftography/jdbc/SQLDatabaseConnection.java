14e97e89a5ca76f8af5bc632ebf6c90683bb4ea1
IvanBuldenko <ivanbwork1@gmail.com>
2021-05-15 14:23:14 +0300

    add prepare statement

--- src/main/java/testgroup/giftography/jdbc/SQLDatabaseConnection.java
+++ src/main/java/testgroup/giftography/jdbc/SQLDatabaseConnection.java
@@ -9,15 +9,26 @@ public class SQLDatabaseConnection {
                         "user=root&" +
                         "password=71498236";
 
-        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
-            // Code here.
-      //  Statement statement = connection.createStatement();
-            System.out.println(connection.isClosed());
-          //  String selectStatus = " select * from status;";
+        try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+
+            String sql = "INSERT INTO client (ProductName, Price) Values (?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from client");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("clientId"));
+                System.out.println(resultSet.getString("firstName"));
+                System.out.println(resultSet.getString("secondName"));
+                System.out.println(resultSet.getString("address"));
+                System.out.println(resultSet.getString("phoneNumber"));
+                System.out.println(resultSet.getString("email"));
+                System.out.println("----------------------");
+            }
         }
-        // Handle any errors that may have occurred.
-        catch (SQLException e) {
-            e.printStackTrace();
+            // Handle any errors that may have occurred.
+        catch(SQLException e){
+                e.printStackTrace();
+            }
         }
     }
-}
